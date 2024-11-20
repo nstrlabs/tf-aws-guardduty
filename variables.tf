060f270602ec0e5173dbf561b7b2ce992991ec36
Fran Rodriguez <fran.clasico@gmail.com>
2024-11-20 13:00:31 +0100

    fix: variable description

--- variables.tf
+++ variables.tf
@@ -26,13 +26,13 @@ variable "enable_kubernetes_protection" {
 }
 
 variable "enable_malware_protection" {
-  description = "Configure and enable Malware Protection as data source for EC2 instances with findings for the detector. Defaults to `true`."
+  description = "Configure and enable Malware Protection as data source for EC2 instances or EKS with findings for the detector. Defaults to `false`."
   type        = bool
-  default     = true
+  default     = false
 }
 
 variable "enable_rds_protection" {
-  description = "Configure and enable RDS protection analyzing login activity for potential access threatsDefaults to `false`."
+  description = "Configure and enable RDS protection analyzing login activity for potential access threats. Defaults to `true`."
   type        = bool
   default     = true
 }
