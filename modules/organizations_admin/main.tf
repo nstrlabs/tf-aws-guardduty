483ed6ac821375124af8bcbb6523c5e92f4368a4
Fran Rodriguez <fran.clasico@gmail.com>
2024-11-22 11:27:46 +0100

    feat: upgrade deprecated resources

--- modules/organizations_admin/main.tf
+++ modules/organizations_admin/main.tf
@@ -12,22 +12,43 @@ resource "aws_guardduty_organization_configuration" "this" {
   auto_enable                      = var.auto_enable_organization_members != null ? null : var.auto_enable_org_config
   auto_enable_organization_members = var.auto_enable_org_config != null ? null : var.auto_enable_organization_members
   detector_id                      = var.guardduty_detector_id
+}
 
-  datasources {
-    s3_logs {
-      auto_enable = var.enable_s3_protection
-    }
-    kubernetes {
-      audit_logs {
-        enable = var.enable_kubernetes_protection
-      }
-    }
-    malware_protection {
-      scan_ec2_instance_with_findings {
-        ebs_volumes {
-          auto_enable = var.enable_malware_protection
-        }
-      }
-    }
+resource "aws_guardduty_organization_configuration_feature" "s3_data_events" {
+  detector_id = aws_guardduty_detector.primary.id
+  name        = "S3_DATA_EVENTS"
+  status      = var.enable_s3_protection ? "ENABLED" : "DISABLED"
+}
+
+resource "aws_guardduty_organization_configuration_feature" "runtime_monitoring" {
+  detector_id = aws_guardduty_detector.primary.id
+  name        = "RUNTIME_MONITORING"
+  status      = var.enable_malware_protection ? "ENABLED" : "DISABLED"
+
+  additional_configuration {
+    name   = "EKS_ADDON_MANAGEMENT"
+    status = var.enable_malware_protection ? "ENABLED" : "DISABLED"
   }
+  additional_configuration {
+    name   = "EC2_AGENT_MANAGEMENT"
+    status = var.enable_malware_protection ? "ENABLED" : "DISABLED"
+  }
+}
+
+resource "aws_guardduty_organization_configuration_feature" "eks_audit_logs" {
+  detector_id = aws_guardduty_detector.primary.id
+  name        = "EKS_AUDIT_LOGS"
+  status      = var.enable_kubernetes_protection ? "ENABLED" : "DISABLED"
+}
+
+resource "aws_guardduty_organization_configuration_feature" "ebs_malware_protection" {
+  detector_id = aws_guardduty_detector.primary.id
+  name        = "EBS_MALWARE_PROTECTION"
+  status      = var.enable_malware_protection ? "ENABLED" : "DISABLED"
+}
+
+resource "aws_guardduty_organization_configuration_feature" "rds_login_events" {
+  detector_id = aws_guardduty_detector.primary.id
+  name        = "RDS_LOGIN_EVENTS"
+  status      = var.enable_rds_protection ? "ENABLED" : "DISABLED"
 }
