df3d064ea3f189fa8045579dc963699ead4c187d
Fran Rodriguez <fran.clasico@gmail.com>
2024-11-29 12:58:36 +0100

    feat: member config

--- modules/organizations_admin/variables.tf
+++ modules/organizations_admin/variables.tf
@@ -105,3 +105,25 @@ variable "auto_enable_organization_members" {
   type        = string
   default     = "NEW"
 }
+
+##################################################
+# GuardDuty Organizations Member
+##################################################
+variable "member_config" {
+  description = <<EOF
+  Specifies the member account configuration:
+  `account_id`                 - The 13 digit ID number of the member account. Example: `123456789012`.
+  `email`                      - Email address to send the invite for member account. Defaults to `null`.
+  `invite`                     - Whether to invite the account to GuardDuty as a member. Defaults to `false`. To detect if an invitation needs to be (re-)sent, the Terraform state value is true based on a relationship_status of `Disabled` | `Enabled` |  `Invited` |  EmailVerificationInProgress.
+  `invitation_message`         - Message for invitation. Defaults to `null`.
+  `disable_email_notification` - Whether an email notification is sent to the accounts. Defaults to `false`.
+  EOF
+  type = list(object({
+    account_id                 = number
+    email                      = string
+    invite                     = bool
+    invitation_message         = optional(string)
+    disable_email_notification = optional(bool)
+  }))
+  default = null
+}
