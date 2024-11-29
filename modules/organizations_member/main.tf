e3a21c91e96c7447697d7a9c650698827db8cb97
Fran Rodriguez <fran.clasico@gmail.com>
2024-11-29 16:17:09 +0100

    fix: remove useless for_each

--- modules/organizations_member/main.tf
+++ modules/organizations_member/main.tf
@@ -4,8 +4,6 @@ data "aws_caller_identity" "current" {}
 # GuardDuty Organizations Invite
 ##################################################
 resource "aws_guardduty_invite_accepter" "member" {
-  for_each = var.member_config != null ? { for member in var.member_config : member.account_id => member if member.invite } : {}
-
   detector_id       = var.guardduty_detector_id
   master_account_id = var.master_account_id == null ? data.aws_caller_identity.current.account_id : var.master_account_id
 }
