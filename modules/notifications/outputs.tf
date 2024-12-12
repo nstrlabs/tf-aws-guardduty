f0e3c159e68e9d01405926a05cabb7d282769c73
Fran Rodriguez <fran.clasico@gmail.com>
2024-12-12 12:35:08 +0100

    fix: outputs

--- modules/notifications/outputs.tf
+++ modules/notifications/outputs.tf
@@ -1,49 +1,50 @@
 output "cloudwatch_event_rule_details" {
   description = "Details of CloudWatch event rule"
-  value = {
-    arn           = aws_cloudwatch_event_rule.main.arn
-    name          = aws_cloudwatch_event_rule.main.name
-    description   = aws_cloudwatch_event_rule.main.description
-    event_pattern = aws_cloudwatch_event_rule.main.event_pattern
-  }
+  value = length(aws_cloudwatch_event_rule.main) > 0 ? {
+    arn           = aws_cloudwatch_event_rule.main[0].arn
+    name          = aws_cloudwatch_event_rule.main[0].name
+    description   = aws_cloudwatch_event_rule.main[0].description
+    event_pattern = aws_cloudwatch_event_rule.main[0].event_pattern
+  } : null
 }
 
 output "cloudwatch_event_target_slack_id" {
   description = "Id of CloudWatch event target for Slack"
-  value       = aws_cloudwatch_event_target.slack.target_id
+  value       = length(aws_cloudwatch_event_target.slack) > 0 ? aws_cloudwatch_event_target.slack[0].target_id : null
 }
 
 output "cloudwatch_event_target_slack_arn" {
   description = "ARN of CloudWatch event target for Slack"
-  value       = aws_cloudwatch_event_target.slack.arn
+  value       = length(aws_cloudwatch_event_target.slack) > 0 ? aws_cloudwatch_event_target.slack[0].arn : null
 }
 
 output "cloudwatch_event_target_pagerduty_id" {
   description = "Id of CloudWatch event target for PagerDuty"
-  value       = aws_cloudwatch_event_target.pagerduty.target_id
+  value       = length(aws_cloudwatch_event_target.pagerduty) > 0 ? aws_cloudwatch_event_target.pagerduty[0].target_id : null
 }
 
 output "cloudwatch_event_target_pagerduty_arn" {
   description = "ARN of CloudWatch event target for PagerDuty"
-  value       = aws_cloudwatch_event_target.pagerduty.arn
+  value       = length(aws_cloudwatch_event_target.pagerduty) > 0 ? aws_cloudwatch_event_target.pagerduty[0].arn : null
 }
 
 output "cloudwatch_event_target_email_id" {
   description = "Id of CloudWatch event target for Email"
-  value       = aws_cloudwatch_event_target.email.target_id
+  value       = length(aws_cloudwatch_event_target.email) > 0 ? aws_cloudwatch_event_target.email[0].target_id : null
 }
 
 output "cloudwatch_event_target_email_arn" {
   description = "ARN of CloudWatch event target for Email"
-  value       = aws_cloudwatch_event_target.email.arn
+  value       = length(aws_cloudwatch_event_target.email) > 0 ? aws_cloudwatch_event_target.email[0].arn : null
 }
 
 output "sns_topic_subscription_details_email" {
-  value = {
-    id                         = aws_sns_topic_subscription.email.id
-    topic_arn                  = aws_sns_topic_subscription.email.topic_arn
-    endpoint                   = aws_sns_topic_subscription.email.endpoint
-    protocol                   = aws_sns_topic_subscription.email.protocol
-    confirmation_authenticated = aws_sns_topic_subscription.email.confirmation_was_authenticated
-  }
+  description = "Details of SNS topic"
+  value = length(aws_sns_topic_subscription.email) > 0 ? {
+    id        = aws_sns_topic_subscription.email[0].id
+    topic_arn = aws_sns_topic_subscription.email[0].topic_arn
+    endpoint  = aws_sns_topic_subscription.email[0].endpoint
+    protocol  = aws_sns_topic_subscription.email[0].protocol
+    confirmed = aws_sns_topic_subscription.email[0].confirmation_was_authenticated
+  } : null
 }
